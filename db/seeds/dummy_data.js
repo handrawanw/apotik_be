bef28193d332b79a0b207c7a020d84f033a67a5f
handrawanw <42910429+handrawanw@users.noreply.github.com>
2023-04-03 14:00:05 +0700

    Update dummy_data.js

--- db/seeds/dummy_data.js
+++ db/seeds/dummy_data.js
@@ -10,7 +10,7 @@ exports.seed = async function(knex) {
   await knex('roles').del()
   
   await knex('users').insert([
-    {id: 1, nama:"wawan", phone:"081314254420", username:"handrawanw", password:"$2a$10$v3XLHBlrFGdrdX7BWhpCvu2Ip1t5GyqkixXSBbUihb8ziwiPb8Ozi", alamat:"JL Teluk Gong Timur"},
+    {id: 1, nama:"wawan", phone:"081234567891", username:"handrawanw", password:"$2a$10$v3XLHBlrFGdrdX7BWhpCvu2Ip1t5GyqkixXSBbUihb8ziwiPb8Ozi", alamat:"JL Teluk Gong Timur"},
   ]);
 
   await knex('roles').insert([
